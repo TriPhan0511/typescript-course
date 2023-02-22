ddb81d2b3411534fe5a7f0a1fe4f0b3be16254f9
Tri Phan <phanphutri@gmail.com>
2023-02-22 20:54:51 +0700

    Updated

--- lesson03/build/js/main.js
+++ lesson03/build/js/main.js
@@ -9,3 +9,49 @@ guitars.unshift('Jim');
 let test = [];
 let bands = [];
 bands.push('Van Halen');
+// Tuple
+let myTuple = ['Dave', 42, true];
+let mixed = ['John', 1, false];
+myTuple[1] = 43;
+// Object
+let myObj;
+myObj = [];
+// console.log(typeof myObj)
+let myArr = ['Hello', 'Aloha'];
+// console.log(typeof myArr)
+myObj = bands;
+myObj = {};
+let exampleObj = {
+    prop1: 'Dave',
+    prop2: true,
+};
+exampleObj.prop2 = false;
+let evh = {
+    name: 'Eddie',
+    active: false,
+    albums: [1984, 5150, 'OU812'],
+};
+let jp = {
+    active: true,
+    albums: ['I', 'II', 'IV'],
+};
+evh = jp;
+// const greetGuitarist = (guitarist: Guitarist) => {
+//   return `Hello, ${guitarist.name?.toUpperCase || 'guitarist'}!`
+// }
+const greetGuitarist = (guitarist) => {
+    if (guitarist.name) {
+        return `Hello, ${guitarist.name}!`;
+    }
+    return 'Hello!';
+};
+console.log(greetGuitarist(jp));
+// Enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
--- lesson03/src/main.ts
+++ lesson03/src/main.ts
@@ -13,3 +13,76 @@ guitars.unshift('Jim')
 let test = []
 let bands: string[] = []
 bands.push('Van Halen')
+
+// Tuple
+let myTuple: [string, number, boolean] = ['Dave', 42, true]
+
+let mixed = ['John', 1, false]
+
+myTuple[1] = 43
+
+// Object
+let myObj: object
+
+myObj = []
+// console.log(typeof myObj)
+
+let myArr: string[] = ['Hello', 'Aloha']
+// console.log(typeof myArr)
+
+myObj = bands
+myObj = {}
+
+let exampleObj = {
+  prop1: 'Dave',
+  prop2: true,
+}
+
+exampleObj.prop2 = false
+
+// type Guitarist = {
+//   name: string
+//   active?: boolean
+//   albums: (string | number)[]
+// }
+
+interface Guitarist {
+  name?: string
+  active: boolean
+  albums: (string | number)[]
+}
+
+let evh: Guitarist = {
+  name: 'Eddie',
+  active: false,
+  albums: [1984, 5150, 'OU812'],
+}
+
+let jp: Guitarist = {
+  active: true,
+  albums: ['I', 'II', 'IV'],
+}
+
+evh = jp
+
+// const greetGuitarist = (guitarist: Guitarist) => {
+//   return `Hello, ${guitarist.name?.toUpperCase || 'guitarist'}!`
+// }
+
+const greetGuitarist = (guitarist: Guitarist) => {
+  if (guitarist.name) {
+    return `Hello, ${guitarist.name}!`
+  }
+  return 'Hello!'
+}
+
+console.log(greetGuitarist(jp))
+
+// Enums
+enum Grade {
+  U = 1,
+  D,
+  C,
+  B,
+  A,
+}

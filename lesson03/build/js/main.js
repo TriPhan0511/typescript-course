4bd996fce2a48ab051a28e6242ad2c642d57816e
Tri Phan <phanphutri@gmail.com>
2023-03-27 20:26:10 +0700

    Updated

--- lesson03/build/js/main.js
+++ lesson03/build/js/main.js
@@ -1,9 +1,9 @@
 "use strict";
-let stringArr = ['one', 'hey', 'Dave'];
+let stringArray = ['one', 'hey', 'Dave'];
 let guitars = ['Strat', 'Les Paul', 5150];
 let mixedData = ['EVH', 1984, true];
-stringArr[0] = 'John';
-stringArr.push('hey');
+stringArray[0] = 'John';
+stringArray.push('hey');
 guitars[0] = 1984;
 guitars.unshift('Jim');
 let test = [];
@@ -11,47 +11,7 @@ let bands = [];
 bands.push('Van Halen');
 // Tuple
 let myTuple = ['Dave', 42, true];
+console.log('🚀 ~ myTuple:', myTuple);
 let mixed = ['John', 1, false];
-myTuple[1] = 43;
-// Object
-let myObj;
-myObj = [];
-// console.log(typeof myObj)
-let myArr = ['Hello', 'Aloha'];
-// console.log(typeof myArr)
-myObj = bands;
-myObj = {};
-let exampleObj = {
-    prop1: 'Dave',
-    prop2: true,
-};
-exampleObj.prop2 = false;
-let evh = {
-    name: 'Eddie',
-    active: false,
-    albums: [1984, 5150, 'OU812'],
-};
-let jp = {
-    active: true,
-    albums: ['I', 'II', 'IV'],
-};
-evh = jp;
-// const greetGuitarist = (guitarist: Guitarist) => {
-//   return `Hello, ${guitarist.name?.toUpperCase || 'guitarist'}!`
-// }
-const greetGuitarist = (guitarist) => {
-    if (guitarist.name) {
-        return `Hello, ${guitarist.name}!`;
-    }
-    return 'Hello!';
-};
-console.log(greetGuitarist(jp));
-// Enums
-var Grade;
-(function (Grade) {
-    Grade[Grade["U"] = 1] = "U";
-    Grade[Grade["D"] = 2] = "D";
-    Grade[Grade["C"] = 3] = "C";
-    Grade[Grade["B"] = 4] = "B";
-    Grade[Grade["A"] = 5] = "A";
-})(Grade || (Grade = {}));
+myTuple[2] = 42;
+console.log('🚀 ~ myTuple:', myTuple);
--- lesson03/src/main.ts
+++ lesson03/src/main.ts
@@ -1,11 +1,11 @@
-let stringArr = ['one', 'hey', 'Dave']
+let stringArray = ['one', 'hey', 'Dave']
 
 let guitars = ['Strat', 'Les Paul', 5150]
 
 let mixedData = ['EVH', 1984, true]
 
-stringArr[0] = 'John'
-stringArr.push('hey')
+stringArray[0] = 'John'
+stringArray.push('hey')
 
 guitars[0] = 1984
 guitars.unshift('Jim')
@@ -16,73 +16,9 @@ bands.push('Van Halen')
 
 // Tuple
 let myTuple: [string, number, boolean] = ['Dave', 42, true]
+console.log('🚀 ~ myTuple:', myTuple)
 
 let mixed = ['John', 1, false]
 
-myTuple[1] = 43
-
-// Object
-let myObj: object
-
-myObj = []
-// console.log(typeof myObj)
-
-let myArr: string[] = ['Hello', 'Aloha']
-// console.log(typeof myArr)
-
-myObj = bands
-myObj = {}
-
-let exampleObj = {
-  prop1: 'Dave',
-  prop2: true,
-}
-
-exampleObj.prop2 = false
-
-// type Guitarist = {
-//   name: string
-//   active?: boolean
-//   albums: (string | number)[]
-// }
-
-interface Guitarist {
-  name?: string
-  active: boolean
-  albums: (string | number)[]
-}
-
-let evh: Guitarist = {
-  name: 'Eddie',
-  active: false,
-  albums: [1984, 5150, 'OU812'],
-}
-
-let jp: Guitarist = {
-  active: true,
-  albums: ['I', 'II', 'IV'],
-}
-
-evh = jp
-
-// const greetGuitarist = (guitarist: Guitarist) => {
-//   return `Hello, ${guitarist.name?.toUpperCase || 'guitarist'}!`
-// }
-
-const greetGuitarist = (guitarist: Guitarist) => {
-  if (guitarist.name) {
-    return `Hello, ${guitarist.name}!`
-  }
-  return 'Hello!'
-}
-
-console.log(greetGuitarist(jp))
-
-// Enums
-enum Grade {
-  U = 1,
-  D,
-  C,
-  B,
-  A,
-}
+myTuple[2] = 42
+console.log('🚀 ~ myTuple:', myTuple)
